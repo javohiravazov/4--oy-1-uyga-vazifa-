80212ff8c2ebac5680c4d419daa58fd1fa379f1e
javohiravazov <skromnie19@gmail.com>
2024-06-23 03:35:44 +0300

    commit-name

---
+++ main.js
@@ -0,0 +1,38 @@
+/*
+{1-masala}
+ var a = 10;
+var b = 5;
+console.log(a + b);
+ */
+
+/*
+2-masala
+var a = 25;
+var b = 15;
+result = a - b;
+console.log(result); 
+*/
+
+/*
+3-masala
+var a = 10;
+var b = 5;
+c = a - b;
+console.log(c);
+var d = 7;
+result = c * d;
+console.log(result);
+*/
+
+/* 
+4-masala
+var a = 10;
+var b = 3;
+console.log(a % b);
+*/
+
+/*
+5-masala
+var number = 2 ** 10;
+console.log(number);
+*/
